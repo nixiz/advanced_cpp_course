e54bba29c1404a94958a3d07cc8f4ea5b873e415
Oguzhan Katli <oguzhan.katli@afiniti.com>
2021-10-11 19:14:57 +0300

    inheritance changes

--- src/1-Inheritance/diamond_problem.cpp
+++ src/1-Inheritance/diamond_problem.cpp
@@ -3,130 +3,161 @@
 #include <set>
 
 namespace diamondproblem {
-  template<typename T> // Template Singleton Implementer
-  class Singleton {
-  protected:
-    Singleton() = default;
-    Singleton(const Singleton&) = delete;
-    Singleton& operator=(const Singleton&) = delete;
-    virtual ~Singleton() = default;
-  public:
-    static T& getInstance()
-    {
-      // thread safety in initialization of static variable guaranteed with C++11 standard
-      static T instance;
-      return instance;
-    }
-  };
-    
-  class Object;
-  class STMFrameworkObjectManager : public Singleton<STMFrameworkObjectManager> {
-    friend class Object;
-  public:
-    void printObjects() const;
-  private:
-    void RegisterObject(Object *obj) {
-      _totalObjects++;
-      registeredObjects.insert(obj);
-    }
-
-    void UnregisterObject(Object *obj) {
-      _totalObjects--;
-      registeredObjects.erase(obj);
-    }
-  private:
-    std::set<Object*> registeredObjects;
-    unsigned long long _totalObjects;
-  };
-
-  class Object {
-  public:
-    explicit Object(const char *name = "Object") : _name(name) {
-      STMFrameworkObjectManager::getInstance().RegisterObject(this);
-    }
-    virtual ~Object() {
-      STMFrameworkObjectManager::getInstance().UnregisterObject(this);
-    }
-    std::string getName() const {
-      return _name;
-    }
-  private:
-    std::string _name;
-  };
-
-  //
-  void STMFrameworkObjectManager::printObjects() const {
-    printf("\ntotal alive objects: %llu", _totalObjects);
-    for (Object* obj : registeredObjects)
-    {
-      printf("\n%s", obj->getName().c_str());
-    }
-  }
-
-
   namespace problem {
+    class Shape {
+    public:
+      explicit Shape(const char* n)
+        : name(n) 
+      { 
+        std::cout << "I'm a(n) " << name << "\n";
+      }
 
-    class ComponentA : public Object {
-    public: ComponentA() : Object("ComponentA") { }
+      void printArea() const {
+        std::cout << "area of "
+          << name << ": "
+          << this->getArea() << "\n";
+      }
+
+      virtual float getArea() const = 0;
+    private:
+      const char* name;
     };
 
-    class ComponentB : public Object {
-    public: ComponentB() : Object("ComponentB") { }
-    };
-#ifdef _WIN32
-#pragma warning( push )
-#pragma warning( disable : 4584) // disable compiler warning for multiple base class inheritance
-#else
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Winaccessible-base"
-#endif
-    class CompositeComponent : public Object, public ComponentA {
-    public: CompositeComponent() : Object("CompositeComponent") { }
-    };
-#ifdef _WIN32
-#pragma warning( pop ) 
-#else
-#pragma GCC diagnostic pop
-#endif
+    class Rectangle : public Shape {
+    public:
+      Rectangle(float w, float h)
+        : Shape("Rectangle")
+        , width(w)
+        , height(h)
+      { }
 
-  }  // namespace problem
-
-  namespace solution {
-
-    class ComponentA : virtual public Object {
-    public: ComponentA() : Object("ComponentA") { }
+      float getArea() const override {
+        return width * height;
+      }
+    protected:
+      float width;
+      float height;
     };
 
-    class ComponentB : virtual public Object {
-    public: ComponentB() : Object("ComponentB") { }
+    class Triangle : public Shape {
+    public:
+      Triangle(float b, float h, float y)
+        : Shape("Triangle")
+        , base(b), height(h), y_angle(y)
+      { }
+
+      float getArea() const override {
+        return (base * height) / 2.f;
+      }
+    protected:
+      float base, height, y_angle;
     };
 
-    class CompositeComponent : public ComponentB, public ComponentA {
-    public: CompositeComponent() : Object("CompositeComponent") { }
+    class ComplexShape : public Triangle, public Rectangle {
+    public:
+      ComplexShape(Rectangle r, Triangle t)
+        //: Shape("Square")
+        : Rectangle(r)
+        , Triangle(t)
+      { }
+
+      float getArea() const override {
+        return width * width;
+      }
     };
 
-  }  // namespace problem
+    void demonstrate()
+    {
+      ComplexShape cs(Rectangle(5, 3), Triangle(5, 2, 12));
+      //cs.printArea();
+      std::cout << "area of complex shape is " << cs.getArea() << "\n";
 
-  void PrintObjects() {
-    STMFrameworkObjectManager::getInstance().printObjects();
-  }
+      //also cannot use polymorphism
+      //Shape* cs_ptr = new ComplexShape(Rectangle(5, 3), Triangle(5, 2, 12));
+    }
+  } // namespace problem
+
+
+  namespace solved {
+    class Shape {
+    public:
+      explicit Shape(const char* n)
+        : name(n) 
+      {
+        std::cout << "I'm a(n) " << name << "\n";
+      }
+      ~Shape() = default;
+
+      void printArea() const {
+        std::cout << "area of "
+          << name << ": "
+          << this->getArea() << "\n";
+      }
+
+      virtual float getArea() const = 0;
+    private:
+      const char* name;
+    };
+
+    class Rectangle : virtual public Shape {
+    public:
+      Rectangle(float w, float h)
+        : Shape("Rectangle")
+        , width(w)
+        , height(h)
+      { }
+
+      float getArea() const override {
+        return width * height;
+      }
+    protected:
+      float width;
+      float height;
+    };
+
+    class Triangle : virtual public Shape {
+    public:
+      Triangle(float b, float h, float y)
+        : Shape("Triangle")
+        , base(b), height(h), y_angle(y)
+      { }
+
+      float getArea() const override {
+        return (base * height) / 2.f;
+      }
+    protected:
+      float base, height, y_angle;
+    };
+
+    class ComplexShape 
+      : public Triangle
+      , public Rectangle 
+    {
+    public:
+      ComplexShape(Rectangle r, Triangle t)
+        : Shape("Square")
+        , Rectangle(r)
+        , Triangle(t)
+      { }
+
+      float getArea() const override {
+        return width * width;
+      }
+    };
+
+    void demonstrate()
+    {
+      ComplexShape cs(Rectangle(5, 3), Triangle(5, 2, 12));
+      cs.printArea();
+      Shape* cs_ptr = new ComplexShape(Rectangle(5, 3), Triangle(5, 2, 12));
+      cs_ptr->printArea();
+
+    }
+  } // namespace problem
 }  // namespace diamondproblem
 
 ELEMENT_CODE(DiamondProblemExample) {
-  namespace dp = diamondproblem;
-  {
-    dp::problem::ComponentA a;
-    dp::problem::ComponentB b;
-    dp::problem::CompositeComponent cc;
-    dp::PrintObjects();
-  }
-  dp::PrintObjects();
-
-  {
-    dp::solution::ComponentA a;
-    dp::solution::ComponentB b;
-    dp::solution::CompositeComponent cc;
-    dp::PrintObjects();
-  }
-  dp::PrintObjects();
+  using namespace diamondproblem;
+  problem::demonstrate();
+  solved::demonstrate();
 }
--- src/1-Inheritance/virtual_destructor.cpp
+++ src/1-Inheritance/virtual_destructor.cpp
@@ -1,88 +1,153 @@
 #include <advanced_cpp_topics.h>
-#include <string.h>
+#include <string>
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <algorithm>
+#include <numeric>
+#include <bitset>
 #include "inc/number.hpp"
 
 namespace virtualdestructor {
-
   namespace problem {
-    class Object {
+
+    class Shape {
     public:
-      Object(const char *name) : _name(name) {
-        printf("\nobject created: %s", _name.c_str());
+      Shape() {
+        std::cout << "Shape Ctor\n";
       }
-      ~Object() {
-        printf("\nobject destructed: %s", _name.c_str());
+      ~Shape() {
+        std::cout << "Shape ~Dtor\n";
+      }
+
+      void printArea() const {
+        std::cout << "area of "
+          << this->getName() << ": "
+          << this->getArea() << "\n";
+      }
+
+      virtual const char* getName() const {
+        return "[undefined shape]";
+      }
+
+      virtual float getArea() const = 0;
+    };
+
+    class Square : public Shape {
+    public:
+      explicit Square(int a)
+        : width(a) 
+      { 
+        std::cout << "Square Ctor\n";
+      }
+      ~Square() {
+        std::cout << "Square ~Dtor\n";
+      }
+
+      float getArea() const override {
+        return (float)(width * width);
+      }
+
+      const char* getName() const override {
+        return "Square";
       }
     private:
-      std::string _name;
+      int width;
     };
+
+    //void printMemoryBlock(unsigned char *mem, size_t len)
+    //{
+    //  auto hex_string = [](unsigned char c) {
+    //    std::stringstream ss;
+    //    ss << std::hex << std::setw(2) << std::setfill('0') << (int)c;
+    //    return ss.str();
+    //  };
+    //  auto dash_fold = [&](std::string a, unsigned char c) {
+    //    return std::move(a) + '-' + hex_string(c);
+    //  };
+    //  std::string s = std::accumulate(std::next(mem + 1), mem + len,
+    //                                  hex_string(mem[0]), // start with first element
+    //                                  dash_fold);
+    //  std::cout << "memory[" << len <<"]: " << s << "\n";
+    //}
+
+    //template <typename T>
+    //void printMemoryBlock(T* ptr, size_t len = 0)
+    //{
+    //  printMemoryBlock(reinterpret_cast<unsigned char*>(ptr), len ? len : sizeof(T));
+    //}
+
+    void demonstrate()
+    {
+      Shape* s = new Square(0x01010101);
+      s->printArea();
+      delete s;
+    }
+
   }
 
   namespace solution {
-    class Object {
+
+    class Shape {
     public:
-      Object(const char *name) : _name(name) {
-        printf("\nobject created: %s", _name.c_str());
+      Shape() {
+        std::cout << "Shape Ctor\n";
       }
-      virtual ~Object() {
-        printf("\nobject destructed: %s", _name.c_str());
+      virtual ~Shape() {
+        std::cout << "Shape ~Dtor\n";
+      }
+
+      void printArea() const {
+        std::cout << "area of "
+          << this->getName() << ": "
+          << this->getArea() << "\n";
+      }
+
+      virtual const char* getName() const {
+        return "[undefined shape]";
+      }
+
+      virtual float getArea() const = 0;
+    };
+
+    class Square : public Shape {
+    public:
+      explicit Square(int a)
+        : width(a)
+      {
+        std::cout << "Square Ctor\n";
+      }
+      ~Square() {
+        std::cout << "Square ~Dtor\n";
+      }
+
+      float getArea() const override {
+        return (float)(width * width);
+      }
+
+      const char* getName() const override {
+        return "Square";
       }
     private:
-      std::string _name;
+      int width;
     };
-  }
 
-  template <class Object>
-  class DerivedFromObject : public Object {
-    Number *_nameSize;
-  public:
-    DerivedFromObject(const char *name = "DerivedFromObject") : Object(name)
+    void demonstrate()
     {
-      printf("\nDerivedFromObject created: %s", name);
-      // ! gcc has a bug for decltype of private member !
-#ifdef _WIN32
-      _nameSize = new Number(static_cast<decltype(Number::n)>(strlen(name)));
-#else
-      _nameSize = new Number(strlen(name));
-#endif
+      Shape* s = new Square(0x01010101);
+      s->printArea();
+      delete s;
     }
-    ~DerivedFromObject()
-    {
-      printf("\nDerivedFromObject destructed");
-      delete _nameSize;
-    }
-  };
+  } // namespace solution
 
-  template <class Object>
-  void Usage() {
-    std::vector<std::unique_ptr<Object>> objectArray;
-    // populate vector
-    for (size_t i = 0; i < 2; i++)
-    {
-      std::ostringstream ss;
-      ss << "object[" << i << "]";
-      objectArray.emplace_back(new DerivedFromObject<Object>(ss.str().c_str()));
-    }
-    // then clear.
-    objectArray.clear();
-  }
-
-  void Problem() {
-    Usage<problem::Object>();
-  }
-
-  void Solution() {
-    Usage<solution::Object>();
-  }
 } // namespace virtualdestructor
 
 ELEMENT_CODE(VirtualDestructorUsage) {
+  using namespace virtualdestructor;
   {
-    virtualdestructor::Problem();
+    problem::demonstrate();
   }
   {
-    virtualdestructor::Solution();
+    solution::demonstrate();
   }
 }
